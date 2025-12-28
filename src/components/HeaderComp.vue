381351ab49a6a8119ccc80d50d358351efc05e0d
BSelyomin <bogdan.selyomin@gmail.com>
2025-12-28 16:31:06 -0500

    Fixed header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+    class="sticky top-0 z-50 w-full border-b bg-black/95 backdrop-blur supports-[backdrop-filter]:bg-black/60"
   >
     <div class="container mx-auto px-4 h-16 flex items-center justify-between">
       <h1 class="font-heading text-3xl tracking-tight uppercase" data-testid="text-logo">
